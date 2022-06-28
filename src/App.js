16bdaa19018e2c50ee64fe37c432b4e7d2815598
vinit <kumarvinit705@gmail.com>
2022-06-28 11:49:26 +0530

    tesla clone

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './components/Header'
+import Home from './components/Home'
+import Section from './components/Section';
+
+function App() {
+
+  return (
+    <>
+    <Header/>
+    <Home/>
+    <Section/>
+    </>
+  );
+}
+
+export default App;
